1c874f4972afdf50de8c59901081011a0612b755
David <arockiadavidl17@gmail.com>
2025-03-25 18:35:38 +0530

    deleted

--- frontend/src/pages/NewAdminProfilePage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AdminProfile from '../components/AdminProfile';
-
-const AdminProfilePage = () => {
-  return <AdminProfile />;
-};
-
-export default AdminProfilePage;
